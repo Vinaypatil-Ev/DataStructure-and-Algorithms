c71cc9f4c940ad711972a2f27f0a5654b053c8f0
Vinaypatil-Ev <vinaypatil153@gmail.com>
2020-09-15 22:32:56 +0530

    15 sep 2020

---
+++ Stack/Java/StackUsingLinkedList/StackLLImpl.java
@@ -0,0 +1,70 @@
+class Node{
+    //
+    Object value;
+    Node next;
+    public Node(Object value, Node next){
+        this.value = value;
+        this.next = next;
+    }
+}
+
+
+class StackLL<T>{
+    Node top;
+    public StackLL(){
+        this.top = null;
+    }
+
+    public void push(Object value){
+        Node newNode = new Node(value, null);
+        if(top == null){
+            top =  newNode;
+        }else{
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+
+    public void pop(){
+        if(top == null){
+            throw new NullPointerException("shift on empty Stack");
+        }
+        if(top.next == null){
+            top = null;
+        }else{
+            top = top.next;   
+        }
+    }
+        
+    public void printStack(){
+        Node temp = top;
+        if (temp == null){
+            System.out.println("Empty list");
+        }
+        while(temp != null){
+            // System.out.println(temp.value);
+            System.out.println(" |" + (T)temp.value + "|");
+            temp = temp.next;
+        }
+    }
+}
+public class StackLLImpl {
+
+    public static void main(String[] args){
+
+        StackLL<Integer> stack = new StackLL<Integer>();
+        stack.push(100);
+        stack.push(9);
+        stack.push(3);
+        stack.push(29);
+        stack.push(99);
+        stack.printStack();
+        stack.pop();
+        System.out.println("Stack on pop");
+        stack.printStack();
+        stack.pop();
+        System.out.println("Stack on pop");
+        stack.printStack();
+    }
+
+}
