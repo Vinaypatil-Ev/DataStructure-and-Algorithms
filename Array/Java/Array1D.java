a0b8bb56c6f5fdf0471f632c6b8cebcc33093f23
Vinaypatil-Ev <vinaypatil153@gmail.com>
2020-11-12 10:54:11 +0530

    12 NOV 2020

--- Array/Java/Array1D.java
+++ Array/Java/Array1D.java
@@ -3,6 +3,10 @@ public class Array1D {
 
     public static void main(String[] args){
 
+        // Array in string
+
+        // Integer Array
+
         int[] NameOfArray = new int[5];
         System.out.println("Length of Array is: "+ NameOfArray.length);     
         int no = 10;
@@ -13,6 +17,20 @@ public class Array1D {
         System.out.println("Elements in Array");
         for (int i=0; i<NameOfArray.length; i++){
             System.out.println(NameOfArray[i]);
+        }
+
+        // float Array
+        
+        float[] floatArr = {3.4f, 4.6f, 6.4f, 5.66f};
+        for (int i = 0; i < floatArr.length; i++) {
+            System.out.println(floatArr[i]);
+        }
+
+        // Double Array
+
+        double[] doubleArr = {3.44, 45.66, 45.908, 30.3};
+        for (int i = 0; i < doubleArr.length; i++) {
+            System.out.println(doubleArr[i]);
         }
 
         
