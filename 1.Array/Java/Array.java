beb6c96f8d2509fa8e6325387b5a07ba7d84e47a
Vinaypatil-Ev <vinaypatil153@gmail.com>
2021-06-28 18:26:17 +0530

    28 June 2021

--- 1.Array/Java/Array.java
+++ 1.Array/Java/Array.java
@@ -10,17 +10,17 @@ class Array {
         //array declaration;
         // datatype arr_name[size] = {values...};
 
-        Integer a[] = {1, 2, 3, 4};
+        Integer a[] = {1, 2, 3, 4}; // int array
         //int aa[] = {1, 2, 3, 4};
-        Float b[] = {1.3f, 3.f, 2.7f, 2f};
+        Float b[] = {1.3f, 3.f, 2.7f, 2f}; // float array
         //float bb[] = {1.3f, 3.f, 2.7f, 2f};
-        Double c[] = {2.44, 9.33, 1.2, 89.9};
+        Double c[] = {2.44, 9.33, 1.2, 89.9}; // double array
         //double cc[] = {2.44, 9.33, 1.2, 89.9};
-        Boolean d[] = {true, false, false, true};
+        Boolean d[] = {true, false, false, true}; // bool arry
         //boolean dd[] = {true, false, false, true};
-        Byte e[] = {1, 3, 4, 5};
+        Byte e[] = {1, 3, 4, 5}; // byte array
         //byte ee[] = {1, 3, 4, 5};
-        String s[] = {"str1", "str2", "str3"};
+        String s[] = {"str1", "str2", "str3"}; // string array
 
         // Two dimentional array
         Integer m[][] = {{1, 2}, {11, 22}, {11, 33}};
